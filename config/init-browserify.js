b6c432e755d73a2c72b080e6676cc67d9743e121
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-21 17:54:28 -0400

    explaining why it is a good idea to explicitly expose backbone and underscore

--- config/init-browserify.js
+++ config/init-browserify.js
@@ -7,21 +7,18 @@
  * backbone and underscore are exposed here in order to include them only once in the bundle for two reasons:
  *  - smaller bundle
  *  - it is desirable to use the same backbone instance across the entire application
- * 
- * TODO: Exposing backbone and underscore may not be necessary once each module
- * installs via npm, since `npm dedupe` may ensure that we always only have one
- * copy of each in our node_modules.
- * Therefore once we reach that stage we should check if that is the case.
  *
+ * Exposing backbone and underscore is not necessary when installing all dependencies and running `npm dedupe` 
+ * afterwards, however in order to support linking of dependencies it is advisable to explicitly expose them here.
+ * 
  * @name exports
  * @function
  * @return {Object} browserify instance
  */
 var go = module.exports = function (browserify) {
   return browserify()
+    .require(require.resolve('./expose-jquery'), { expose: 'jquery' })
     .require(require.resolve('underscore'), { expose: 'underscore' })
     .require(require.resolve('backbone'), { expose: 'backbone' })
-    .require(require.resolve('./expose-jquery'), { expose: 'jquery' })
-    .require(require.resolve('../client/core'), { expose: 'ses-core' })
     ;
 };
