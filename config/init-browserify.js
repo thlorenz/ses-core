34e630b0b97fc39232612a7ceae0e181c5115f67
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-28 11:55:10 -0400

    removing init-browerify code related to dedupe

--- config/init-browserify.js
+++ config/init-browserify.js
@@ -2,6 +2,7 @@
 
 /**
  * Initializes browserify instance and adds and exposes certain client side libraries with it.
+ * Supply in the appup-config: `exports.initBrowserify = core.initBrowserify;`
  *
  * jquery is exposed in order to be able to do `var $ = require('jquery');`
  * backbone and underscore are exposed here in order to include them only once in the bundle for two reasons:
@@ -16,9 +17,10 @@
  * @return {Object} browserify instance
  */
 var go = module.exports = function (browserify) {
-  return browserify()
+  var s = browserify()
     .require(require.resolve('./expose-jquery'), { expose: 'jquery' })
     .require(require.resolve('underscore'), { expose: 'underscore' })
     .require(require.resolve('backbone'), { expose: 'backbone' })
     ;
+  return s;
 };
